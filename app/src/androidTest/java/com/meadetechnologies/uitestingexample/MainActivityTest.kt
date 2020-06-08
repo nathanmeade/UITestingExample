2d36cdb4c85d0782cc7a66b5ede32269bd3c8cc7
Nathan Meade <nathan.r.meade@gmail.com>
2020-06-08 12:16:58 -0400

    feat: Input testing implemented

--- app/src/androidTest/java/com/meadetechnologies/uitestingexample/MainActivityTest.kt
+++ app/src/androidTest/java/com/meadetechnologies/uitestingexample/MainActivityTest.kt
@@ -1,54 +1,42 @@
 package com.meadetechnologies.uitestingexample
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.app.Instrumentation.*
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.os.Bundle
-import android.provider.MediaStore
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
-import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.platform.app.InstrumentationRegistry
-import com.meadetechnologies.uitestingexample.ImageViewHasDrawableMatcher.hasDrawable
 import org.hamcrest.CoreMatchers.not
-import org.hamcrest.Matcher
-import org.junit.Assert.*
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
 
-    @get: Rule
-    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
-
     @Test
-    fun test_cameraIntent_isBitmapSetToImageView() {
-        val activityResult = createImageCaptureActivityResultStub()
-        val expectedIntent: Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
-        intending(expectedIntent).respondWith(activityResult)
+    fun test_showDialog_captureNameInput() {
 
-        // Execute and verify
-        onView(withId(R.id.image)).check(matches(not(hasDrawable())))
-        onView(withId(R.id.button_launch_camera)).perform(click())
-        intending(expectedIntent)
-        onView(withId(R.id.image)).check(matches(hasDrawable()))
-    }
+        //given
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val EXPECTED_NAME = "Mitch"
 
-    private fun createImageCaptureActivityResultStub(): ActivityResult? {
-        val bundle = Bundle()
-        bundle.putParcelable(KEY_IMAGE_DATA, BitmapFactory.decodeResource(intentsTestRule.activity.resources, R.drawable.ic_launcher_background))
-        val resultData = Intent()
-        resultData.putExtras(bundle)
-        return ActivityResult(Activity.RESULT_OK, resultData)
+        //Execute and verify
+        onView(withId(R.id.button_launch_dialog)).perform(click())
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        onView(withText(R.string.text_ok)).perform(click())
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        //enter some input
+        onView(withId(R.id.md_input_message)).perform(typeText(EXPECTED_NAME))
+        onView(withText(R.string.text_ok)).perform(click())
+
+        //make sure dialog is gone
+        onView(withText(R.string.text_enter_name)).check(doesNotExist())
+
+        //make sure name changed values
+        onView(withId(R.id.text_name)).check(matches(withText(EXPECTED_NAME)))
     }
 }
