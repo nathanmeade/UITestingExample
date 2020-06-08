57e6ac3025f9d5d46264e13b81303494d86cd37d
Nathan Meade <nathan.r.meade@gmail.com>
2020-06-08 11:42:51 -0400

    feat: ImageViews, Drawable Matchers and Bitmaps testing implemented

--- app/src/androidTest/java/com/meadetechnologies/uitestingexample/MainActivityTest.kt
+++ app/src/androidTest/java/com/meadetechnologies/uitestingexample/MainActivityTest.kt
@@ -1,43 +1,54 @@
 package com.meadetechnologies.uitestingexample
 
-import androidx.test.core.app.ActivityScenario
+import android.app.Activity
+import android.app.Instrumentation
+import android.app.Instrumentation.*
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.MediaStore
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.internal.runner.junit4.AndroidJUnit4Builder
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import com.meadetechnologies.uitestingexample.ImageViewHasDrawableMatcher.hasDrawable
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matcher
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest{
+class MainActivityTest {
+
+    @get: Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Test
-    fun test_isActivityInView() {
+    fun test_cameraIntent_isBitmapSetToImageView() {
+        val activityResult = createImageCaptureActivityResultStub()
+        val expectedIntent: Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
+        intending(expectedIntent).respondWith(activityResult)
 
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.main)).check(matches(isDisplayed()))
+        // Execute and verify
+        onView(withId(R.id.image)).check(matches(not(hasDrawable())))
+        onView(withId(R.id.button_launch_camera)).perform(click())
+        intending(expectedIntent)
+        onView(withId(R.id.image)).check(matches(hasDrawable()))
     }
 
-    @Test
-    fun test_visibility_title_nextButton() {
-
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed())) // method 1
-
-        onView(withId(R.id.button_next_activity)).check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method 2
-    }
-
-    @Test
-    fun test_isTitleTextDisplayed() {
-
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)))
-
-
+    private fun createImageCaptureActivityResultStub(): ActivityResult? {
+        val bundle = Bundle()
+        bundle.putParcelable(KEY_IMAGE_DATA, BitmapFactory.decodeResource(intentsTestRule.activity.resources, R.drawable.ic_launcher_background))
+        val resultData = Intent()
+        resultData.putExtras(bundle)
+        return ActivityResult(Activity.RESULT_OK, resultData)
     }
 }
--- app/src/main/java/com/meadetechnologies/uitestingexample/MainActivity.kt
+++ app/src/main/java/com/meadetechnologies/uitestingexample/MainActivity.kt
@@ -1,19 +1,52 @@
 package com.meadetechnologies.uitestingexample
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val REQUEST_IMAGE_CAPTURE = 1234
+const val KEY_IMAGE_DATA = "data"
+
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "AppDebug"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_next_activity.setOnClickListener {
-            val intent = Intent(this, SecondaryActivity::class.java)
-            startActivity(intent)
+        button_launch_camera.setOnClickListener {
+            dispatchCameraIntent()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            Log.d(TAG, "RESULT_OK")
+            when(requestCode){
+
+                REQUEST_IMAGE_CAPTURE -> {
+                    Log.d(TAG, "REQUEST_IMAGE_CAPTURE detected.")
+                    data?.extras.let{ extras ->
+                        if (extras == null || !extras.containsKey(KEY_IMAGE_DATA)) {
+                            return
+                        }
+                        val imageBitmap = extras[KEY_IMAGE_DATA] as Bitmap?
+                        image.setImageBitmap(imageBitmap)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun dispatchCameraIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
 }
